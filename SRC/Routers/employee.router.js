ad780f54a2dd917b6966155655b02dc936a8efa6
mario malak <93185168+mariomalak1@users.noreply.github.com>
2024-12-12 18:04:23 +0200

    search query

--- SRC/Routers/employee.router.js
+++ SRC/Routers/employee.router.js
@@ -1,3 +1,10 @@
 import {Router} from "express";
 
+import {addEmpolyee, getAllEmpolyees} from "../Controllers/employee.controller.js";
+
 export const router = Router();
+
+router.route("/")
+    .get(getAllEmpolyees)
+    .post(addEmpolyee);
+
